b45a1b7353cd818a3114a24e1a13359d72a9d85b
Ronit Rudra <ronit.rudra1995@gmail.com>
2017-05-31 19:34:12 -0500

    Update Lose Panel
    Added Quit to main menu and fixed timescale.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,6 +5,7 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name){
 		Debug.Log ("New Level load: " + name);
+		Time.timeScale = 1.0f;
 		Application.LoadLevel (name);
 	}
 
